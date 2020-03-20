4e3da3da4dac601fd5e6518c2a87f990ad5e257b
gioele-bra <brancaleoni.gioele@iisgalvanimi.edu.it>
2020-03-20 15:55:17 +0000

    Punto 2

---
+++ auto-brancaleoni/src/app/driving-list/driving-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Drive } from '../driving.model';
+
+@Component({
+  selector: 'app-driving-list',
+  templateUrl: './driving-list.component.html',
+  styleUrls: ['./driving-list.component.css']
+})
+export class DrivingListComponent implements OnInit {
+
+  driveList : Drive[] = new Array<Drive>();
+
+  constructor(public http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<Drive[]>("https://my-json-server.typicode.com/malizia-g/fine_anno_exp/mezzi").subscribe(
+      (auto: Drive[]) => {
+        this.driveList = auto;
+      }
+    )
+  }
+
+}
